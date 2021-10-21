2a74e8f493a9af981bf58b4154c5713044df30e6
abhijith br <abhijithpoojari1122@gmail.com>
2021-10-21 19:28:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+  };
+  
+  const Article = mongoose.model("Article", articleSchema);
+
+  //fetch all articles
+app.get("/articles", function(req, res){
+    Article.find(function(err, foundArticles){
+        res.send(foundArticles);
+    });
+});
+
+//create one new artcicle
+app.post("/articles", function(req, res){
+const newArticle= new Article({
+    title: req.body.title,
+    content: req.body.content 
+});
+newArticle.save(function(err){
+    if(!err){
+        res.send("successfully added");
+    }else{
+        res.send(err);
+    }
+});
+});
+
+
+//delete all article
+app.delete("/articles", function(req, res){
+    Article.deleteMany(function(err){
+      if (!err){
+        res.send("Successfully deleted all the articles in wikiDB.");
+      } else {
+        res.send(err);
+      }
+    });
+  
+  });
+
+//TODO
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
